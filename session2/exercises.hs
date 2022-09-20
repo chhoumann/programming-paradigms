2b83b8c6a25abac6368a6c3fd15e5928ed6c848d
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-09-20 19:44:00 +0200

    session 2 exercises

---
+++ session2/exercises.hs
@@ -0,0 +1,13 @@
+twice :: (f -> f) -> f -> f
+twice f x = f(f x)
+
+dingo (x, y) = [x, y]
+
+mango x y z = x * y + z - 42
+
+bingo a = a
+
+bighead :: (Num a, Ord a) => [a] -> Int
+bighead xs = count
+    where   h = head xs
+            count = length [e | e <- tail xs, e > h]
