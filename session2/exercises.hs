ef1a847babf45730b0840ef9d8c5f50ce7a648fd
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-09-21 10:10:43 +0200

    map fn

--- session2/exercises.hs
+++ session2/exercises.hs
@@ -11,3 +11,10 @@ bighead :: (Num a, Ord a) => [a] -> Int
 bighead xs = count
     where   h = head xs
             count = length [e | e <- tail xs, e > h]
+
+
+-- Exercise F
+mapp :: (i -> o) -> [i] -> [o]
+mapp f xs = [f x | x <- xs]
+
+double n = 2*n
