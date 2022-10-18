5a1fe919bb4a85a477932983832628bf2a7aa250
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-10-18 12:56:54 +0200

    make session folder script

---
+++ mkses.sh
@@ -0,0 +1,22 @@
+if [ -z "$1" ]; then
+    echo "Usage: mkses.sh <lecture number>"
+    exit 1
+fi
+
+# Create the directory
+dir="session$1"
+mkdir $dir
+
+# Copy the template files
+echo "module Session$1Spec where
+    
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import SolutionSession$1
+
+main :: IO()
+main = hspec $ do
+    describe \"Session$1.placeholder\" $ do\n" > $dir/test.hs
+
+echo "module SolutionSession$1(\n\n) where\n" > $dir/SolutionSession$1.hs
