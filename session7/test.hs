8b4eb84bcde584582b420e7f2f40056521f26c77
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-11-15 12:00:03 +0100

    opt out

--- session7/test.hs
+++ session7/test.hs
@@ -7,5 +7,7 @@ import SolutionSession7
 
 main :: IO()
 main = hspec $ do
-    describe "Session7.placeholder" $ do
+    describe "Session7.triples" $ do
+        it "handles exercise test case" $ do
+            triples [(1,2,3), (4,5,6), (7,8,9)] `shouldBe` ([1,4,7], [2,5,8], [3,6,9])
 
