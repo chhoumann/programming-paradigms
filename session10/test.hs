7b547681502ffe028222e42ea8adaf0cf6ca248b
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-01-11 17:22:52 +0100

    session 10

---
+++ session10/test.hs
@@ -0,0 +1,11 @@
+module Session10Spec where
+    
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import SolutionSession10
+
+main :: IO()
+main = hspec $ do
+    describe "Session10.placeholder" $ do
+
