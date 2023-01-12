c3435654b67c6f05c382088bd6758cb86ffb901b
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-01-12 14:17:21 +0100

    session 13

---
+++ session13/SolutionSession13.hs
@@ -0,0 +1,15 @@
+module SolutionSession13 where
+
+{-
+DISCUSSION EXERCISES - EXERCISE 1
+
+Prove by induction on lists that 
+    length (xs ++ ys) = length xs + length ys
+for all lists xs and ys
+-}
+{-
+This is intuitively correct by the commutative law.
+-}
+
+
+-- Not doing this
---
+++ session13/test.hs
@@ -0,0 +1,11 @@
+module Session13Spec where
+    
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import SolutionSession13
+
+main :: IO()
+main = hspec $ do
+    describe "Session13.placeholder" $ do
+
